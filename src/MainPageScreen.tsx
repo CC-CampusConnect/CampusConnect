ba97e725d60f051d3c49f138c316e4001cd96dee
jiseon park <83346782+kimminsu31415@users.noreply.github.com>
2023-11-17 21:15:03 +0900

    feat & design: 메인페이지 sns 버튼, 매칭 시작 버튼, 로그아웃 버튼

--- src/MainPageScreen.tsx
+++ src/MainPageScreen.tsx
@@ -20,12 +20,22 @@ export default function MainPageScreen({navigation}: {navigation: any}) {
     const logout = async () => {
         try {
             await auth().signOut();
-            console.log('로그아웃 되었습니다.');
-            navigation.navigate('Home');
+            console.log('로그아웃 되었습니다. 로그인 페이지로 이동합니다.');
+            navigation.navigate('SignIn');
             
         } catch (error: any) {
             console.log(error);
         }
+    };
+
+    // 카카오톡 계정 입력
+    const snsKakao = async () => {
+        console.log('카카오톡 계정을 입력해 주세요');
+    };
+
+    // 인스타 계정 입력
+    const snsInsta = async () => {
+        console.log('인스타 계정을 입력해 주세요');
     };
 
 
@@ -35,25 +45,47 @@ export default function MainPageScreen({navigation}: {navigation: any}) {
         width: 270,
         height: 334,
         },
+
+        snsImage:{
+            width: 35,
+            height: 35,
+        },
     });
 
     return(
         <View className='flex w-full h-full relative bg-white-gray'>
+            <TouchableOpacity
+                className='absolute top-0 right-5'
+                onPress={logout}>
+                <Text className='mx-auto mt-[16px] mb-[144px] text-xs text-black underline'>로그아웃</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+                className='absolute top-5 ml-[20px]'
+                onPress={snsKakao}>
+                <Image 
+                    style={styles.snsImage}
+                    source={require('images/kakao.png')}/>
+            </TouchableOpacity>
+
+            <TouchableOpacity 
+                className='absolute top-5 ml-[70px]'
+                onPress={snsInsta}>
+                <Image 
+                    style={styles.snsImage}
+                    source={require('images/insta.png')}/>
+            </TouchableOpacity>
+
             <Image 
                 className='mx-auto mt-[91px]'
                 style={styles.LogoImage}
                 source={require('images/CCLogo.png')}/>
-
-            <View className='flex w-full h-full absolute justify-end mb-0'>
-                <TouchableOpacity
-                    className='w-full w-full justify-end h-[65px] bg-pink-500'
-                    onPress={handleSubmit(onSubmit)}>
-                    <Text className='mx-auto my-auto font-sans text-sm text-white'>매칭 시작</Text>
-                </TouchableOpacity>
-            </View>
+            
             <TouchableOpacity
-                onPress={logout}>
-                <Text className='mx-auto mt-[16px] mb-[144px] text-xs text-black underline'>로그아웃</Text>
+                className='mx-auto mt-[160px] w-[217px] h-[217px] bg-pink-500 rounded-full shadow'
+                onPress={handleSubmit(onSubmit)}>
+                <Text className='mx-auto mt-auto font-sans text-xxl text-white'>매칭</Text>
+                <Text className='mx-auto mb-auto font-sans text-xxl text-white'>시작</Text>
             </TouchableOpacity>
         </View>
     )
