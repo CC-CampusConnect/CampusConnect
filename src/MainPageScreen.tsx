46f5a74c64d2a24a2031ec269fbfc4ed84d5e09c
seon5 <wlsl1839@naver.com>
2023-11-23 17:13:16 +0900

    feat: 로그인, 로그아웃 시 context에 uid값 변경
    로그인 시 유저의 UID가 context로 관리하는 uid에 저장되고,
    로그아웃 시 uid를 null값으로 변경한다.

--- src/MainPageScreen.tsx
+++ src/MainPageScreen.tsx
@@ -11,18 +11,21 @@ import {
   Image,
   StyleSheet,
   ScrollView,
-  Modal, 
-  Alert, 
+  Modal,
+  Alert,
 } from 'react-native';
 import {useForm, Controller} from 'react-hook-form';
 import auth from '@react-native-firebase/auth';
 import {db} from './util/firestore';
+import {useContext} from 'react';
+import {IsLoginContext} from './IsLoginContext';
 
 export default function MainPageScreen({navigation}: {navigation: any}) {
   const {control, handleSubmit, formState} = useForm<FormData>();
   const [text, setText] = useState<string>(''); // 기타 사유
   const [modalVisible, setModalVisible] = useState(false); // 카카오톡 모달 상태
   const [modalVisible2, setModalVisible2] = useState(false); // 인스타 모달 상태
+  const {setUid} = useContext(IsLoginContext);
 
   // 매칭 시작 버튼 누르면 WebRTCRoom으로 이동
   const onSubmit = async (data: FormData) => {
@@ -34,6 +37,8 @@ export default function MainPageScreen({navigation}: {navigation: any}) {
   const logout = async () => {
     try {
       await auth().signOut();
+      // 로그아웃 시 uid를 null로 변경
+      setUid(null);
       console.log('로그아웃 되었습니다. 로그인 페이지로 이동합니다.');
       navigation.navigate('SignIn');
     } catch (error: any) {
@@ -54,7 +59,6 @@ export default function MainPageScreen({navigation}: {navigation: any}) {
     }
   };
 
-  
   // 인스타 클릭 시 호출되는 함수
   const instahandleReport = async () => {
     try {
@@ -85,16 +89,40 @@ export default function MainPageScreen({navigation}: {navigation: any}) {
     <View className="flex w-full h-full relative bg-white-gray">
       {/* 배경 이미지 */}
       <View className="w-[346px] h-[767px] left-[33px] top-[30px] absolute">
-        <Image className="w-[46px] h-[35px] left-0 top-0 absolute" source={require('images/Cat.png')} />
-        <Image className="w-[46px] h-[35px] left-[6px] top-[402px] absolute" source={require('images/Cat.png')} />
-        <Image className="w-[46px] h-[35px] left-[6px] top-[692px] absolute" source={require('images/Cat.png')} />
+        <Image
+          className="w-[46px] h-[35px] left-0 top-0 absolute"
+          source={require('images/Cat.png')}
+        />
+        <Image
+          className="w-[46px] h-[35px] left-[6px] top-[402px] absolute"
+          source={require('images/Cat.png')}
+        />
+        <Image
+          className="w-[46px] h-[35px] left-[6px] top-[692px] absolute"
+          source={require('images/Cat.png')}
+        />
 
-        <Image className="w-16 h-9 left-[155px] top-[191px] absolute" source={require('images/Rabbit.png')} />
-        <Image className="w-16 h-9 left-[155px] top-[582px] absolute" source={require('images/Rabbit.png')} />
-        
-        <Image className="w-[53px] h-[35px] left-[293px] top-[69px] absolute" source={require('images/Dog.png')} />
-        <Image className="w-[53px] h-[35px] left-[293px] top-[470px] absolute" source={require('images/Dog.png')} />
-        <Image className="w-[53px] h-[35px] left-[293px] top-[732px] absolute" source={require('images/Dog.png')} />
+        <Image
+          className="w-16 h-9 left-[155px] top-[191px] absolute"
+          source={require('images/Rabbit.png')}
+        />
+        <Image
+          className="w-16 h-9 left-[155px] top-[582px] absolute"
+          source={require('images/Rabbit.png')}
+        />
+
+        <Image
+          className="w-[53px] h-[35px] left-[293px] top-[69px] absolute"
+          source={require('images/Dog.png')}
+        />
+        <Image
+          className="w-[53px] h-[35px] left-[293px] top-[470px] absolute"
+          source={require('images/Dog.png')}
+        />
+        <Image
+          className="w-[53px] h-[35px] left-[293px] top-[732px] absolute"
+          source={require('images/Dog.png')}
+        />
       </View>
 
       {/* 카카오톡 계정 입력 모달 */}
@@ -102,45 +130,72 @@ export default function MainPageScreen({navigation}: {navigation: any}) {
         <Modal
           visible={modalVisible}
           // 안드로이드의 뒤로가기 버튼 클릭 시 모달 비활성화
-          onRequestClose={() => {setModalVisible(!modalVisible);}}
-          animationType="slide"
-        >
+          onRequestClose={() => {
+            setModalVisible(!modalVisible);
+          }}
+          animationType="slide">
           {/* 배경 이미지 */}
           <View className="w-[346px] h-[767px] left-[33px] top-[30px] absolute">
-            <Image className="w-[46px] h-[35px] left-0 top-0 absolute" source={require('images/Cat.png')} />
-            <Image className="w-[46px] h-[35px] left-[6px] top-[402px] absolute" source={require('images/Cat.png')} />
-            <Image className="w-[46px] h-[35px] left-[6px] top-[692px] absolute" source={require('images/Cat.png')} />
+            <Image
+              className="w-[46px] h-[35px] left-0 top-0 absolute"
+              source={require('images/Cat.png')}
+            />
+            <Image
+              className="w-[46px] h-[35px] left-[6px] top-[402px] absolute"
+              source={require('images/Cat.png')}
+            />
+            <Image
+              className="w-[46px] h-[35px] left-[6px] top-[692px] absolute"
+              source={require('images/Cat.png')}
+            />
 
-            <Image className="w-16 h-9 left-[155px] top-[191px] absolute" source={require('images/Rabbit.png')} />
-            <Image className="w-16 h-9 left-[155px] top-[582px] absolute" source={require('images/Rabbit.png')} />
-            
-            <Image className="w-[53px] h-[35px] left-[293px] top-[69px] absolute" source={require('images/Dog.png')} />
-            <Image className="w-[53px] h-[35px] left-[293px] top-[470px] absolute" source={require('images/Dog.png')} />
-            <Image className="w-[53px] h-[35px] left-[293px] top-[732px] absolute" source={require('images/Dog.png')} />
+            <Image
+              className="w-16 h-9 left-[155px] top-[191px] absolute"
+              source={require('images/Rabbit.png')}
+            />
+            <Image
+              className="w-16 h-9 left-[155px] top-[582px] absolute"
+              source={require('images/Rabbit.png')}
+            />
+
+            <Image
+              className="w-[53px] h-[35px] left-[293px] top-[69px] absolute"
+              source={require('images/Dog.png')}
+            />
+            <Image
+              className="w-[53px] h-[35px] left-[293px] top-[470px] absolute"
+              source={require('images/Dog.png')}
+            />
+            <Image
+              className="w-[53px] h-[35px] left-[293px] top-[732px] absolute"
+              source={require('images/Dog.png')}
+            />
           </View>
 
-          <View className='top-[178px] mx-auto w-[391px] h-[207px] bg-white border'>
-            <Text 
+          <View className="top-[178px] mx-auto w-[391px] h-[207px] bg-white border">
+            <Text
               style={{fontFamily: 'GowunDodum-Regular'}}
-              className='mx-auto text-[26px] mt-[22px] ml-[29px] text-brown'>
-                카카오톡
+              className="mx-auto text-[26px] mt-[22px] ml-[29px] text-brown">
+              카카오톡
             </Text>
             <TextInput
               className="w-[330px] h-[52px] ml-[30px] mt-[20px] border border-solid-100 bg-white-gray"
               value={text}
-              onChangeText={t => {setText(t);}}
+              onChangeText={t => {
+                setText(t);
+              }}
               placeholder="카카오톡 계정을 입력해주세요."
             />
-            <View className='flex flex-row mx-auto mt-[20px] space-x-24'>
+            <View className="flex flex-row mx-auto mt-[20px] space-x-24">
               <TouchableOpacity onPress={() => setModalVisible(false)}>
-                <Text 
+                <Text
                   className="mx-auto text-[16px] text-black underline"
                   style={{fontFamily: 'GowunDodum-Regular'}}>
-                    취소
+                  취소
                 </Text>
               </TouchableOpacity>
               <TouchableOpacity onPress={kakaohandleReport}>
-                <Text 
+                <Text
                   className="mx-auto text-[16px] text-black underline"
                   style={{fontFamily: 'GowunDodum-Regular'}}>
                   확인
@@ -156,45 +211,72 @@ export default function MainPageScreen({navigation}: {navigation: any}) {
         <Modal
           visible={modalVisible2}
           // 안드로이드의 뒤로가기 버튼 클릭 시 모달 비활성화
-          onRequestClose={() => {setModalVisible2(!modalVisible2);}}
-          animationType="slide"
-        >
+          onRequestClose={() => {
+            setModalVisible2(!modalVisible2);
+          }}
+          animationType="slide">
           {/* 배경 이미지 */}
           <View className="w-[346px] h-[767px] left-[33px] top-[30px] absolute">
-            <Image className="w-[46px] h-[35px] left-0 top-0 absolute" source={require('images/Cat.png')} />
-            <Image className="w-[46px] h-[35px] left-[6px] top-[402px] absolute" source={require('images/Cat.png')} />
-            <Image className="w-[46px] h-[35px] left-[6px] top-[692px] absolute" source={require('images/Cat.png')} />
+            <Image
+              className="w-[46px] h-[35px] left-0 top-0 absolute"
+              source={require('images/Cat.png')}
+            />
+            <Image
+              className="w-[46px] h-[35px] left-[6px] top-[402px] absolute"
+              source={require('images/Cat.png')}
+            />
+            <Image
+              className="w-[46px] h-[35px] left-[6px] top-[692px] absolute"
+              source={require('images/Cat.png')}
+            />
 
-            <Image className="w-16 h-9 left-[155px] top-[191px] absolute" source={require('images/Rabbit.png')} />
-            <Image className="w-16 h-9 left-[155px] top-[582px] absolute" source={require('images/Rabbit.png')} />
-            
-            <Image className="w-[53px] h-[35px] left-[293px] top-[69px] absolute" source={require('images/Dog.png')} />
-            <Image className="w-[53px] h-[35px] left-[293px] top-[470px] absolute" source={require('images/Dog.png')} />
-            <Image className="w-[53px] h-[35px] left-[293px] top-[732px] absolute" source={require('images/Dog.png')} />
+            <Image
+              className="w-16 h-9 left-[155px] top-[191px] absolute"
+              source={require('images/Rabbit.png')}
+            />
+            <Image
+              className="w-16 h-9 left-[155px] top-[582px] absolute"
+              source={require('images/Rabbit.png')}
+            />
+
+            <Image
+              className="w-[53px] h-[35px] left-[293px] top-[69px] absolute"
+              source={require('images/Dog.png')}
+            />
+            <Image
+              className="w-[53px] h-[35px] left-[293px] top-[470px] absolute"
+              source={require('images/Dog.png')}
+            />
+            <Image
+              className="w-[53px] h-[35px] left-[293px] top-[732px] absolute"
+              source={require('images/Dog.png')}
+            />
           </View>
 
-          <View className='top-[178px] mx-auto w-[391px] h-[207px] bg-white border'>
-            <Text 
+          <View className="top-[178px] mx-auto w-[391px] h-[207px] bg-white border">
+            <Text
               style={{fontFamily: 'GowunDodum-Regular'}}
-              className='mx-auto text-[26px] mt-[22px] ml-[29px] text-brown'>
-                인스타
+              className="mx-auto text-[26px] mt-[22px] ml-[29px] text-brown">
+              인스타
             </Text>
             <TextInput
               className="w-[330px] h-[52px] ml-[30px] mt-[20px] border border-solid-100 bg-white-gray"
               value={text}
-              onChangeText={t => {setText(t);}}
+              onChangeText={t => {
+                setText(t);
+              }}
               placeholder="인스타 계정을 입력해주세요."
             />
-            <View className='flex flex-row mx-auto mt-[20px] space-x-24'>
+            <View className="flex flex-row mx-auto mt-[20px] space-x-24">
               <TouchableOpacity onPress={() => setModalVisible2(false)}>
-                <Text 
+                <Text
                   className="mx-auto text-[16px] text-black underline"
                   style={{fontFamily: 'GowunDodum-Regular'}}>
-                    취소
+                  취소
                 </Text>
               </TouchableOpacity>
               <TouchableOpacity onPress={instahandleReport}>
-                <Text 
+                <Text
                   className="mx-auto text-[16px] text-black underline"
                   style={{fontFamily: 'GowunDodum-Regular'}}>
                   확인
@@ -211,11 +293,15 @@ export default function MainPageScreen({navigation}: {navigation: any}) {
         </Text>
       </TouchableOpacity>
 
-      <TouchableOpacity className="absolute top-5 ml-[20px]" onPress={() => setModalVisible(true)}>
+      <TouchableOpacity
+        className="absolute top-5 ml-[20px]"
+        onPress={() => setModalVisible(true)}>
         <Image style={styles.snsImage} source={require('images/kakao.png')} />
       </TouchableOpacity>
 
-      <TouchableOpacity className="absolute top-5 ml-[70px]" onPress={() => setModalVisible2(true)}>
+      <TouchableOpacity
+        className="absolute top-5 ml-[70px]"
+        onPress={() => setModalVisible2(true)}>
         <Image style={styles.snsImage} source={require('images/insta.png')} />
       </TouchableOpacity>
 
@@ -239,7 +325,6 @@ export default function MainPageScreen({navigation}: {navigation: any}) {
           시작
         </Text>
       </TouchableOpacity>
-      
     </View>
   );
 }
