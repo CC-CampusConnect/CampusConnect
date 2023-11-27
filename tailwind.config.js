dc2a92eda27a0659b04944b7c6a47b27bf58c8b1
stanaly <tlsgusrb70@gmail.com>
2023-11-27 18:15:34 +0900

    fix: tailwind.config 내 pink-500 중복제거

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,34 +10,33 @@ module.exports = {
 
         /* Text 색상 토큰 */
         'orange-600': '#FF9730',
-        'brown':'#643D18',
-        'white':'#FFFFFF', /* +) Background-Button 토큰 */
-        'black':'#000000', /* +) Background-Button 토큰 */
-        'gray':'#A6A6A6',
-        'red':'#B00020',
+        brown: '#643D18',
+        white: '#FFFFFF' /* +) Background-Button 토큰 */,
+        black: '#000000' /* +) Background-Button 토큰 */,
+        gray: '#A6A6A6',
+        red: '#B00020',
 
         /* Background 색상 토큰 */
-        'white-gray':'#F8F8F8',
-        'pink-500':'#FF8967',
-        'gray-200':'#DBDBDB', /* +) Background-Button 토큰 */
-        'orange-400':'#FFAA5B', /* +) Background-Button 토큰 */
-
+        'white-gray': '#F8F8F8',
+        'pink-500': '#FF8967',
+        'gray-200': '#DBDBDB' /* +) Background-Button 토큰 */,
+        'orange-400': '#FFAA5B' /* +) Background-Button 토큰 */,
 
         /* Background-Button 색상 토큰 */
-        'pink-500':'#FF8967',
+        'pink-500': '#FF8967',
       },
 
       /* 폰트 크기 토큰 */
-      fontSize:{
-        'xxl':'45px',
-        'xl':'40px',
-        'lg':'32px',
-        'md':'26px',
-        'sm':'20px',
-        'ssm':'16px',
-        'xs':'14px',
+      fontSize: {
+        xxl: '45px',
+        xl: '40px',
+        lg: '32px',
+        md: '26px',
+        sm: '20px',
+        ssm: '16px',
+        xs: '14px',
       },
-    },   
+    },
   },
   plugins: [],
 };
